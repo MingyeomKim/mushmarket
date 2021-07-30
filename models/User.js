478807f8239dc445a78c7e2888597f89853bb60c
mingyeom <mingyeom119@naver.com>
2021-07-30 16:26:57 +0900

    Bcrypt 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // salt의 크기를 지정
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +33,29 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  // 비밀번호를 암호화 시킨다.
+  var user = this; // user은 User 스키마를 가리킴
+
+  // 다른 요소가 아닌 password가 변경되었을 때만 비밀번호가 암호화되어 저장되도록 한다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds.toExponential, function (err, salt) {
+      if (err) return next(err); // next로 이동하면 바로 save함수가 실행
+      // salt가 잘 생성되면 아래 문구가 실행
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next(); // save로 돌아가기
+      });
+      //user.password : user 스키마의 plain password(가공되지 않은 비밀번호)
+      // salt : 생성된 salt
+      // hash : 암호화된 비밀번호
+    });
+  }
+});
+
+// user정보를 저장하기 전에 function을 실행하게끔
 
 const User = mongoose.model("User", userSchema);
 // 모듈의 사용성을 늘리기 위해 exports
